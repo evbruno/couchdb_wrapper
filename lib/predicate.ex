0418f4c4e36f1f8994d88c86792582188adc3a72
Eduardo V. Bruno <eduardo.bruno@salesfloor.net>
2024-09-23 14:23:25 -0300

    fix predicates

--- lib/predicate.ex
+++ lib/predicate.ex
@@ -2,82 +2,90 @@ defmodule Predicate do
   def eval(p, obj) do
     # Thanks CoPilot for your hardwork ! 🤖
     case p do
-      {field, :eq, value} -> Map.get(obj, field) == value
-      {field, :ne, value} -> Map.get(obj, field) != value
-      {field, :gt, value} -> Map.get(obj, field) > value
-      {field, :gte, value} -> Map.get(obj, field) >= value
-      {field, :lt, value} -> Map.get(obj, field) < value
-      {field, :lte, value} -> Map.get(obj, field) <= value
-      {field, :in, values} -> Enum.member?(values, Map.get(obj, field))
-      {field, :nin, values} -> !Enum.member?(values, Map.get(obj, field))
+      # string representation
+      {field, :eq, value} -> field_path(obj, field) |> to_string == value
+      {field, :ne, value} -> field_path(obj, field) |> to_string != value
+      {field, :in, values} -> Enum.member?(values, field_path(obj, field) |> to_string)
+      {field, :nin, values} -> !Enum.member?(values, field_path(obj, field) |> to_string)
+      {field, :regex, regex} -> Regex.match?(~r/#{regex}/, field_path(obj, field) |> to_string)
+      # cast to number
+      {field, :gt, value} -> field_path(obj, field) > value
+      {field, :gte, value} -> field_path(obj, field) >= value
+      {field, :lt, value} -> field_path(obj, field) < value
+      {field, :lte, value} -> field_path(obj, field) <= value
+      {field, :mod, {divisor, remainder}} -> rem(field_path(obj, field), divisor) == remainder
+      # string based operations
+      {field, :elem, index} -> Enum.at(field_path(obj, field), index) != nil
+      {field, :size, size} -> length(field_path(obj, field)) == size
+      # else....
       {field, :exists} -> Map.has_key?(obj, field)
-      {field, :regex, regex} -> Regex.match?(~r/#{regex}/, Map.get(obj, field))
-      {field, :elem, index} -> Enum.at(Map.get(obj, field), index) != nil
-      {field, :size, size} -> length(Map.get(obj, field)) == size
-      {field, :mod, {divisor, remainder}} -> rem(Map.get(obj, field), divisor) == remainder
     end
   end
 
   def parse(str) do
     case String.split(str, ~r/:/, trim: true) do
       [field, "eq", value] ->
-        {String.to_existing_atom(field), :ne, String.to_integer(value)}
+        {field, :eq, value}
 
       [field, "ne", value] ->
-        {String.to_existing_atom(field), :ne, String.to_integer(value)}
+        {field, :ne, value}
 
       [field, "gt", value] ->
-        {String.to_existing_atom(field), :gt, String.to_integer(value)}
+        {field, :gt, String.to_integer(value)}
 
       [field, "gte", value] ->
-        {String.to_existing_atom(field), :gte, String.to_integer(value)}
+        {field, :gte, String.to_integer(value)}
 
       [field, "lt", value] ->
-        {String.to_existing_atom(field), :lt, String.to_integer(value)}
+        {field, :lt, String.to_integer(value)}
 
       [field, "lte", value] ->
-        {String.to_existing_atom(field), :lte, String.to_integer(value)}
+        {field, :lte, String.to_integer(value)}
 
       [field, "in", values] ->
-        {String.to_existing_atom(field), :in,
-         String.split(values, ",") |> Enum.map(&String.to_integer/1)}
+        # |> Enum.map(&String.to_integer/1)}
+        {field, :in, String.split(values, ",", trim: true)}
 
       [field, "nin", values] ->
-        {String.to_existing_atom(field), :nin,
-         String.split(values, ",") |> Enum.map(&String.to_integer/1)}
+        # |> Enum.map(&String.to_integer/1)}
+        {field, :nin, String.split(values, ",", trim: true)}
 
       [field, "exists"] ->
-        {String.to_existing_atom(field), :exists}
+        {field, :exists}
 
       [field, "regex", regex] ->
-        {String.to_existing_atom(field), :regex, regex}
+        {field, :regex, regex}
 
       [field, "elem", index] ->
-        {String.to_existing_atom(field), :elem, String.to_integer(index)}
+        {field, :elem, String.to_integer(index)}
 
       [field, "size", size] ->
-        {String.to_existing_atom(field), :size, String.to_integer(size)}
+        {field, :size, String.to_integer(size)}
 
       [field, "mod", divisor, remainder] ->
-        {String.to_existing_atom(field), :mod,
-         {String.to_integer(divisor), String.to_integer(remainder)}}
+        {field, :mod, {String.to_integer(divisor), String.to_integer(remainder)}}
 
       _ ->
         raise ArgumentError, "Invalid predicate -->#{str}<--"
     end
   end
 
-  def get_nested(obj, keys) when is_bitstring(keys),
-    do: get_nested(obj, String.split(keys, ".", trim: true))
+  def field_path(obj, keys), do:
+    # TODO: Kernel.get_in/2 does not work for `get_in(%{"foo" => %{"bar" => "11"}}, ["foo", "bar"])`
+    get_nested(obj, String.split(keys, ".", trim: true))
 
-  def get_nested(obj, keys) do
-    Enum.reduce(keys, obj, fn key, acc ->
-      case acc do
-        nil -> nil
-        _ -> Map.get(acc, key)
-      end
-    end)
+  defp get_nested(obj, _) when not is_map(obj),
+    do: nil
+
+  defp get_nested(obj, [key | tail]) when length(tail) > 0 do
+    with x when not is_nil(x) <- Map.get(obj, key) do
+      get_nested(x, tail)
+    else
+      _ -> nil
+    end
   end
+
+  defp get_nested(obj, [key]), do: Map.get(obj, key)
 
   def combine_actions([]), do: fn _ -> true end
 
--- test/predicate_test.exs
+++ test/predicate_test.exs
@@ -1,181 +1,215 @@
 defmodule PredicateTest do
   use ExUnit.Case, async: true
 
-  describe "get_nested/2 (fetch nested fields)" do
+  describe "field_path/2 (fetch nested fields)" do
     test "existent paths" do
-      assert Predicate.get_nested(%{"age" => 20}, ["age"]) == 20
-      assert Predicate.get_nested(%{age: 20}, [:age]) == 20
+      assert Predicate.field_path(%{"age" => 20}, "age") == 20
 
-      assert Predicate.get_nested(
-               %{"doc" => %{"age" => 20}},
-               ["doc", "age"]
-             ) == 20
-
-      assert Predicate.get_nested(
+      assert Predicate.field_path(
                %{"doc" => %{"age" => 20}},
                "doc.age"
              ) == 20
 
-      assert Predicate.get_nested(
+      assert Predicate.field_path(
                %{"doc" => %{"age" => %{"year" => 1981, "month" => 12}}},
                "doc.age.year"
              ) == 1981
     end
 
     test "nonexistent paths" do
-      assert Predicate.get_nested(%{"age" => 20}, ["year"]) == nil
-      assert Predicate.get_nested(%{age: 20}, [:year]) == nil
+      assert Predicate.field_path(%{"age" => 20}, "year") == nil
 
-      assert Predicate.get_nested(
+      assert Predicate.field_path(
                %{"doc" => %{"age" => 20}},
-               ["doc", "year"]
+               "doc.year"
              ) == nil
 
-      assert Predicate.get_nested(
-               %{"doc" => %{"age" => 20}},
-               ["xdoc", "year"]
-             ) == nil
-
-      assert Predicate.get_nested(
+      assert Predicate.field_path(
                %{"doc" => %{"age" => 20}},
                "xdoc.age"
              ) == nil
 
-      assert Predicate.get_nested(
+      assert Predicate.field_path(
                %{"doc" => %{"age" => %{"year" => 1981, "month" => 12}}},
                "doc.age.yearx"
              ) == nil
 
-      assert Predicate.get_nested(
+      assert Predicate.field_path(
                %{"doc" => %{"age" => %{"year" => 1981, "month" => 12}}},
                "doc.agex.year"
              ) == nil
     end
   end
 
+  defp parse_eval(input, obj) do
+    Predicate.eval(Predicate.parse(input), obj)
+  end
+
+  describe "parse and eval" do
+    test "bar" do
+      assert parse_eval("name:eq:John Doe", %{"name" => "John Doe"})
+      assert parse_eval("age:eq:42", %{"age" => "42"})
+      assert parse_eval("age:eq:42", %{"age" => 42})
+    end
+  end
+
   describe "eval/2" do
-    test "predicate :in" do
-      obj = %{age: 25}
-      assert Predicate.eval({:age, :in, [25, 30]}, obj)
-      refute Predicate.eval({:age, :in, [30, 40]}, obj)
-    end
+    test "predicate :eq" do
+      assert Predicate.eval({"age", :eq, "25"}, %{"age" => "25"})
+      assert Predicate.eval({"age", :eq, "25"}, %{"age" => 25})
 
-    test "predicate :nin" do
-      obj = %{age: 25}
-      assert Predicate.eval({:age, :nin, [30, 40]}, obj)
-      refute Predicate.eval({:age, :nin, [25, 30]}, obj)
-    end
+      refute Predicate.eval({"age", :eq, "30"}, %{"age" => "25"})
 
-    test "predicate :gt" do
-      obj = %{age: 25}
-      assert Predicate.eval({:age, :gt, 20}, obj)
-      refute Predicate.eval({:age, :gt, 30}, obj)
-    end
-
-    test "predicate :gte" do
-      obj = %{age: 25}
-      assert Predicate.eval({:age, :gte, 25}, obj)
-      assert Predicate.eval({:age, :gte, 20}, obj)
-      refute Predicate.eval({:age, :gte, 30}, obj)
-    end
-
-    test "predicate :lt" do
-      obj = %{age: 25}
-      assert Predicate.eval({:age, :lt, 30}, obj)
-      refute Predicate.eval({:age, :lt, 20}, obj)
-    end
-
-    test "predicate :lte" do
-      obj = %{age: 25}
-      assert Predicate.eval({:age, :lte, 25}, obj)
-      assert Predicate.eval({:age, :lte, 30}, obj)
-      refute Predicate.eval({:age, :lte, 20}, obj)
+      assert Predicate.eval({"age.month", :eq, "12"}, %{"age" => %{"month" => "12"}})
+      assert Predicate.eval({"age.month", :eq, "12"}, %{"age" => %{"month" => 12}})
     end
 
     test "predicate :ne" do
-      obj = %{age: 25}
-      assert Predicate.eval({:age, :ne, 30}, obj)
-      refute Predicate.eval({:age, :ne, 25}, obj)
+      assert Predicate.eval({"age", :ne, "30"}, %{"age" => 25})
+      assert Predicate.eval({"age", :ne, "30"}, %{"age" => "25"})
+      assert Predicate.eval({"age.month", :ne, "12"}, %{"age" => %{"month" => 11}})
+
+      refute Predicate.eval({"age", :ne, "25"}, %{"age" => 25})
+      refute Predicate.eval({"age", :ne, "25"}, %{"age" => "25"})
+    end
+
+    test "predicate :in" do
+      obj = %{"age" => 25}
+      assert Predicate.eval({"age", :in, ["25", "30"]}, obj)
+      refute Predicate.eval({"age", :in, ["30, 40"]}, obj)
+
+      assert Predicate.eval(
+               {"age.month", :in, ["a", "b", "11"]},
+               %{"age" => %{"month" => "11"}}
+             )
+    end
+
+    test "predicate :nin" do
+      obj = %{"age" => 25}
+      assert Predicate.eval({"age", :nin, ["30", "40"]}, obj)
+      refute Predicate.eval({"age", :nin, ["25", "30"]}, obj)
+
+      assert Predicate.eval(
+               {"foo.bar", :nin, ["a", "b", "12"]},
+               %{"foo" => %{"bar" => "11"}}
+             )
+
+      assert Predicate.eval(
+               {"foo.bar.buz", :nin, ["a", "b", "12"]},
+               %{"foo" => %{"bar" => "11"}}
+             )
+    end
+
+    test "predicate :gt" do
+      obj = %{"age" => 25}
+      assert Predicate.eval({"age", :gt, 20}, obj)
+      refute Predicate.eval({"age", :gt, 30}, obj)
+    end
+
+    test "predicate :gte" do
+      obj = %{"age" => 25}
+      assert Predicate.eval({"age", :gte, 25}, obj)
+      assert Predicate.eval({"age", :gte, 20}, obj)
+      refute Predicate.eval({"age", :gte, 30}, obj)
+    end
+
+    test "predicate :lt" do
+      obj = %{"age" => 25}
+      assert Predicate.eval({"age", :lt, 30}, obj)
+      refute Predicate.eval({"age", :lt, 20}, obj)
+    end
+
+    test "predicate :lte" do
+      obj = %{"age" => 25}
+      assert Predicate.eval({"age", :lte, 25}, obj)
+      assert Predicate.eval({"age", :lte, 30}, obj)
+      refute Predicate.eval({"age", :lte, 20}, obj)
     end
 
     test "predicate :exists" do
-      obj = %{age: 25}
-      assert Predicate.eval({:age, :exists}, obj)
-      refute Predicate.eval({:name, :exists}, obj)
+      obj = %{"age" => 25}
+      assert Predicate.eval({"age", :exists}, obj)
+      refute Predicate.eval({"name", :exists}, obj)
     end
 
     test "predicate :regex" do
-      obj = %{name: "John Doe"}
-      assert Predicate.eval({:name, :regex, "John"}, obj)
-      refute Predicate.eval({:name, :regex, "Jane"}, obj)
+      obj = %{"name" => "John Doe"}
+      assert Predicate.eval({"name", :regex, "John"}, obj)
+      assert Predicate.eval({"id", :regex, "^1\\d+"}, %{"id" => 1024})
+
+      refute Predicate.eval({"name", :regex, "Jane"}, obj)
     end
 
     test "predicate :elem" do
-      obj = %{list: [10, 20, 30]}
-      assert Predicate.eval({:list, :elem, 1}, obj)
-      refute Predicate.eval({:list, :elem, 3}, obj)
+      obj = %{"list" => [10, 20, 30]}
+      assert Predicate.eval({"list", :elem, 1}, obj)
+      refute Predicate.eval({"list", :elem, 3}, obj)
     end
 
     test "predicate :size" do
-      obj = %{list: [1, 2, 3]}
-      assert Predicate.eval({:list, :size, 3}, obj)
-      refute Predicate.eval({:list, :size, 2}, obj)
+      obj = %{"list" => [1, 2, 3]}
+      assert Predicate.eval({"list", :size, 3}, obj)
+      refute Predicate.eval({"list", :size, 2}, obj)
     end
 
     test "predicate :mod" do
-      obj = %{num: 10}
-      assert Predicate.eval({:num, :mod, {2, 0}}, obj)
-      refute Predicate.eval({:num, :mod, {3, 0}}, obj)
+      obj = %{"num" => 10}
+      assert Predicate.eval({"num", :mod, {2, 0}}, obj)
+      refute Predicate.eval({"num", :mod, {3, 0}}, obj)
     end
   end
 
   describe "parser/1" do
-    test "parses :in predicate" do
-      assert Predicate.parse("age:in:25,30,35") == {:age, :in, [25, 30, 35]}
-    end
-
-    test "parses :nin predicate" do
-      assert Predicate.parse("age:nin:25,30,35") == {:age, :nin, [25, 30, 35]}
-    end
-
-    test "parses :gt predicate" do
-      assert Predicate.parse("age:gt:25") == {:age, :gt, 25}
-    end
-
-    test "parses :gte predicate" do
-      assert Predicate.parse("age:gte:25") == {:age, :gte, 25}
-    end
-
-    test "parses :lt predicate" do
-      assert Predicate.parse("age:lt:25") == {:age, :lt, 25}
-    end
-
-    test "parses :lte predicate" do
-      assert Predicate.parse("age:lte:25") == {:age, :lte, 25}
+    test "parses :eq predicate" do
+      assert Predicate.parse("age:eq:25") == {"age", :eq, "25"}
     end
 
     test "parses :ne predicate" do
-      assert Predicate.parse("age:ne:25") == {:age, :ne, 25}
+      assert Predicate.parse("age:ne:25") == {"age", :ne, "25"}
+    end
+
+    test "parses :in predicate" do
+      assert Predicate.parse("age:in:25,30,35") == {"age", :in, ["25", "30", "35"]}
+    end
+
+    test "parses :nin predicate" do
+      assert Predicate.parse("age:nin:25,30,35") == {"age", :nin, ["25", "30", "35"]}
+    end
+
+    test "parses :gt predicate" do
+      assert Predicate.parse("age:gt:25") == {"age", :gt, 25}
+    end
+
+    test "parses :gte predicate" do
+      assert Predicate.parse("age:gte:25") == {"age", :gte, 25}
+    end
+
+    test "parses :lt predicate" do
+      assert Predicate.parse("age:lt:25") == {"age", :lt, 25}
+    end
+
+    test "parses :lte predicate" do
+      assert Predicate.parse("age:lte:25") == {"age", :lte, 25}
     end
 
     test "parses :exists predicate" do
-      assert Predicate.parse("age:exists") == {:age, :exists}
+      assert Predicate.parse("age:exists") == {"age", :exists}
     end
 
     test "parses :regex predicate" do
-      assert Predicate.parse("name:regex:^John") == {:name, :regex, "^John"}
+      assert Predicate.parse("name:regex:^John") == {"name", :regex, "^John"}
     end
 
     test "parses :elem predicate" do
-      assert Predicate.parse("list:elem:1") == {:list, :elem, 1}
+      assert Predicate.parse("list:elem:1") == {"list", :elem, 1}
     end
 
     test "parses :size predicate" do
-      assert Predicate.parse("list:size:3") == {:list, :size, 3}
+      assert Predicate.parse("list:size:3") == {"list", :size, 3}
     end
 
     test "parses :mod predicate" do
-      assert Predicate.parse("num:mod:2:0") == {:num, :mod, {2, 0}}
+      assert Predicate.parse("num:mod:2:0") == {"num", :mod, {2, 0}}
     end
 
     test "raises ArgumentError for invalid predicate" do
@@ -187,24 +221,28 @@ defmodule PredicateTest do
 
   describe "combine/1" do
     test "empty list of actions" do
-      assert Predicate.combine_actions([]).(%{age: 25})
+      assert Predicate.combine_actions([]).(%{"age" => 25})
       assert Predicate.combine_actions([]).(nil)
     end
 
     test "single action" do
-      assert Predicate.combine_actions([{:age, :ne, 30}]).(%{age: 20})
+      assert Predicate.combine_actions([{"age", :ne, 30}]).(%{"age" => 20})
     end
 
     test "multiple action" do
-      assert Predicate.combine_actions([{:age, :ne, 30}, {:age, :gte, 20}]).(%{age: 20})
+      assert Predicate.combine_actions([{"age", :ne, 30}, {"age", :gte, 20}]).(%{"age" => 20})
 
-      assert Predicate.combine_actions([{:age, :ne, 30}, {:age, :gte, 20}, {:age, :mod, {5, 0}}]).(
-               %{age: 20}
-             )
+      assert Predicate.combine_actions([
+               {"age", :ne, 30},
+               {"age", :gte, 20},
+               {"age", :mod, {5, 0}}
+             ]).(%{"age" => 20})
 
-      refute Predicate.combine_actions([{:age, :ne, 30}, {:age, :gte, 20}, {:age, :mod, {5, 1}}]).(
-               %{age: 20}
-             )
+      refute Predicate.combine_actions([
+               {"age", :ne, 30},
+               {"age", :gte, 20},
+               {"age", :mod, {5, 1}}
+             ]).(%{"age" => 20})
     end
   end
 end
